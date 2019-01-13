06d8e9e294f88e8e18d38465c2fbaa883750dddf
krylenko <krylenko>
2019-01-13 00:08:00 -0800

    add LFO speed up/down function

--- tpMIDIpad.ino
+++ tpMIDIpad.ino
@@ -6,16 +6,19 @@
 #define MIDI_CHANNEL        1
 
 #define LFO_OUT             3
+#define LFO_SPD_DIVISIONS   3
 #define LFO_CC              116
 uint8_t * lfoPtr = tri;
 uint8_t lfoPhase = 0;
 uint16_t lfoSpeed = 5;
+double lfoSpdDivisions[LFO_SPD_DIVISIONS] = {25.0, 10.0, 1.0};
+uint8_t lfoSpdIdx = 1;
 double lfoDepth = 1.0;
 uint8_t lfoDownsampleFactor = 10;   // ramp this up for noise waveforms
 
 // define switch and button (momentary) inputs
-#define NUM_SWITCHES        3 
-enum{
+#define NUM_SWITCHES        3
+enum {
   TOGGLE_LOWER = 5,
   TOGGLE_TOP_LEFT = 9,
   TOGGLE_TOP_RIGHT = 10
@@ -30,12 +33,12 @@ uint16_t switchOld[NUM_SWITCHES];
 
 #define NUM_BUTTONS         5
 #define CHANGE_VALID_CT     5
-enum{
+enum {
   PUSH_ROUND = 6,
   BUTTON_RIGHT_TOP = 7,
   BUTTON_RIGHT_BOTTOM = 8,
   BUTTON_LEFT_BOTTOM = 11,
-  BUTTON_LEFT_TOP = 12  
+  BUTTON_LEFT_TOP = 12
 };
 int buttons[NUM_BUTTONS] = {
   PUSH_ROUND,
@@ -51,7 +54,7 @@ bool sendLow[NUM_BUTTONS];
 
 // define analog inputs
 #define NUM_ANALOG_INPUTS   6
-enum{
+enum {
   PAD_X,
   PAD_Y,
   EXP_PEDAL,
@@ -60,33 +63,33 @@ enum{
   POT_BROWN_TOP
 };
 
-// reference 
-// MIDI message structure:  http://midi.org/techspecs/midimessages.php#3 
+// reference
+// MIDI message structure:  http://midi.org/techspecs/midimessages.php#3
 // controller assignments: http://nickfever.com/music/midi-cc-list
 
-int controllers[NUM_ANALOG_INPUTS + NUM_SWITCHES + NUM_BUTTONS] = { 
-  102,      // A0     pad X               CC 102 
-  103,      // A1     pad Y               CC 103 
-  104,      // A2     exp pedal           CC 104 
-  105,      // A3     top knob            CC 105 
+int controllers[NUM_ANALOG_INPUTS + NUM_SWITCHES + NUM_BUTTONS] = {
+  102,      // A0     pad X               CC 102
+  103,      // A1     pad Y               CC 103
+  104,      // A2     exp pedal           CC 104
+  105,      // A3     top knob            CC 105
   106,      // A4     left knob top       CC 106
   107,      // A5     left knob bot       CC 107
   108,      // D5     lower toggle        CC 108
-  109,      // D6     round push          CC 109  
+  109,      // D6     round push          CC 109
   110,      // D7     right gray but top  CC 110
-  111,      // D8     right gray but bot  CC 111      
+  111,      // D8     right gray but bot  CC 111
   112,      // D9     toggle top left     CC 112
   113,      // D10    toggle top right    CC 113
   114,      // D11    left gray but bot   CC 114
-  115       // D12    left gray but top   CC 115    
-};  
-uint16_t analogVal[NUM_ANALOG_INPUTS]; 
+  115       // D12    left gray but top   CC 115
+};
+uint16_t analogVal[NUM_ANALOG_INPUTS];
 uint16_t analogOld[NUM_ANALOG_INPUTS];
 
 /* The format of the message to send via serial. We create a new data type, that can store 4 values at once.  This will be easier to send as MIDI. */
 typedef struct {
   uint8_t commChannel;  // first  4 bits : channel message (NOTE_ON, NOTE_OFF or CC (controlchange)
-                        // second 4 bits : midi channel (0-15)
+  // second 4 bits : midi channel (0-15)
   uint8_t data1;        // second byte   : first value (0-127), controller number or note number
   uint8_t data2;        // third  byte   : second value (0-127), controller value or velocity
 } t_midiMsg;
@@ -94,8 +97,8 @@ typedef struct {
 uint8_t ctIdx = 0;
 t_midiMsg msg;
 
-uint16_t maxPadInt_x = 585;  
-uint16_t maxPadInt_y = 678;   
+uint16_t maxPadInt_x = 585;
+uint16_t maxPadInt_y = 678;
 
 void setup()
 {
@@ -115,22 +118,22 @@ void setup()
   for (int k = 0; k < NUM_BUTTONS; ++k) {
     pinMode(buttons[k], INPUT);
     highCt[k] = 0;
-    lowCt[k] = 0;    
+    lowCt[k] = 0;
     sendHigh[k] = false;
-    sendLow[k] = false;    
-  }  
+    sendLow[k] = false;
+  }
 
-  Serial.begin(31250);   
-  delay(2000);           
-  
+  Serial.begin(31250);
+  delay(2000);
+
 }
 
 void loop()
 {
 
   static uint8_t lfoDownsampleCt = 0;
-  
-  /************** WRITE MIDI CONTROLLER DATA *********************/ 
+
+  /************** WRITE MIDI CONTROLLER DATA *********************/
 
   // read and output analog MIDI controllers
   uint16_t currVal = 0;
@@ -138,69 +141,69 @@ void loop()
   currVal = analogRead(ctIdx);
 
   msg.commChannel = CC;
-  msg.commChannel += MIDI_CHANNEL - 1; 
+  msg.commChannel += MIDI_CHANNEL - 1;
 
-  switch(ctIdx)
+  switch (ctIdx)
   {
     case PAD_X:
-    {
-      delay(7);
-      currVal = analogRead(ctIdx);    
-      if ( currVal >= minPadInt && currVal <= maxPadInt_x - 20 ) {
+      {
+        delay(7);
+        currVal = analogRead(ctIdx);
+        if ( currVal >= minPadInt && currVal <= maxPadInt_x - 20 ) {
           analogVal[ctIdx] = map(currVal, minPadInt, maxPadInt_x - 20, 0, 127);
           analogVal[ctIdx] = constrain(analogVal[ctIdx], 0, 127);
+        }
+        break;
       }
-      break;
-    }    
     case PAD_Y:
-    {
-      delay(7);
-      currVal = analogRead(ctIdx);    
-      if ( currVal >= minPadInt - 5 && currVal <= maxPadInt_y - 5 ) {
-        analogVal[ctIdx] = map(currVal, maxPadInt_y - 5, minPadInt - 5, 0, 127);
-      }   
-      break;
-    }
+      {
+        delay(7);
+        currVal = analogRead(ctIdx);
+        if ( currVal >= minPadInt - 5 && currVal <= maxPadInt_y - 5 ) {
+          analogVal[ctIdx] = map(currVal, maxPadInt_y - 5, minPadInt - 5, 0, 127);
+        }
+        break;
+      }
     case POT_BROWN_BOTTOM:
-    {
-      analogVal[ctIdx] = currVal >> 3;
-      lfoDepth = double(currVal/1023.0);
-      break;      
-    }
-    case POT_TOP_RIGHT:
-    {
-      analogVal[ctIdx] = currVal >> 3;
-      lfoSpeed = int(currVal / 10.0) + 1;
-      break;      
-    }    
+      {
+        analogVal[ctIdx] = currVal >> 3;
+        lfoDepth = double(currVal / 1023.0);
+        break;
+      }
+    case POT_BROWN_TOP:
+      {
+        analogVal[ctIdx] = currVal >> 3;
+        lfoSpeed = int(currVal / lfoSpdDivisions[lfoSpdIdx]) + 1;
+        break;
+      }
     default:
-    {
-      analogVal[ctIdx] = currVal >> 3;
-      break;
-    }
+      {
+        analogVal[ctIdx] = currVal >> 3;
+        break;
+      }
   }
-  
-  if (analogVal[ctIdx] != analogOld[ctIdx]) {                                  
-    msg.data1 = controllers[ctIdx];               
-    msg.data2 = analogVal[ctIdx];                 
+
+  if (analogVal[ctIdx] != analogOld[ctIdx]) {
+    msg.data1 = controllers[ctIdx];
+    msg.data2 = analogVal[ctIdx];
     Serial.write(msg.commChannel);
     Serial.write(msg.data1);
     Serial.write(msg.data2);
-    analogOld[ctIdx] = analogVal[ctIdx];                   
+    analogOld[ctIdx] = analogVal[ctIdx];
   }
-  
+
   ++ctIdx;
-  if( ctIdx >= NUM_ANALOG_INPUTS ) {
+  if ( ctIdx >= NUM_ANALOG_INPUTS ) {
     ctIdx = 0;
   }
 
   // read and write states of switches
   uint8_t binValOne = 0, binValTwo = 0;
-  for (int m = 0; m < NUM_SWITCHES; ++m) {                    
+  for (int m = 0; m < NUM_SWITCHES; ++m) {
     int tempVal = digitalRead(switches[m]);
-    tempVal == HIGH ? tempVal = 127 : tempVal = 0;                
-    switchVal[m] = tempVal;        
-    if ( switchVal[m] != switchOld[m] ) {           
+    tempVal == HIGH ? tempVal = 127 : tempVal = 0;
+    switchVal[m] = tempVal;
+    if ( switchVal[m] != switchOld[m] ) {
       msg.data1 = controllers[m + NUM_ANALOG_INPUTS];
       msg.data2 = switchVal[m];
       Serial.write(msg.commChannel);
@@ -212,7 +215,7 @@ void loop()
       switchVal[m] == 127 ? binValOne = 1 : binValOne = 0;
     }
     if (switches[m] == TOGGLE_TOP_LEFT) {
-      switchVal[m] == 127 ? binValTwo = 1 : binValTwo = 0; 
+      switchVal[m] == 127 ? binValTwo = 1 : binValTwo = 0;
     }
   }
 
@@ -221,17 +224,31 @@ void loop()
     if ( digitalRead(buttons[p]) ) {
       ++highCt[p];
     } else {
-      if( highCt > 0 ) {
+      if ( highCt > 0 ) {
         ++lowCt[p];
       }
     }
     if (highCt[p] == CHANGE_VALID_CT) {
       sendHigh[p] = true;
+      if (buttons[p] == BUTTON_LEFT_TOP) {
+        // raise LFO speed
+        ++lfoSpdIdx;
+        if (lfoSpdIdx >= LFO_SPD_DIVISIONS) {
+          lfoSpdIdx = LFO_SPD_DIVISIONS - 1;
+        }
+      }
+      else if (buttons[p] == BUTTON_LEFT_BOTTOM) {
+        // lower LFO speed
+        --lfoSpdIdx;
+        if (lfoSpdIdx <= 0) {
+          lfoSpdIdx = 0;
+        }
+      }
     }
     if (lowCt[p] == CHANGE_VALID_CT) {
       sendLow[p] = true;
     }
-        
+
     if (sendHigh[p]) {
       msg.data1 = controllers[p + NUM_ANALOG_INPUTS + NUM_SWITCHES];
       msg.data2 = 127;
@@ -240,7 +257,7 @@ void loop()
       Serial.write(msg.data2);
       sendHigh[p] = false;
       lowCt[p] = 0;
-    } 
+    }
     if (sendLow[p]) {
       msg.data1 = controllers[p + NUM_ANALOG_INPUTS + NUM_SWITCHES];
       msg.data2 = 0;
@@ -249,36 +266,36 @@ void loop()
       Serial.write(msg.data2);
       sendLow[p] = false;
       highCt[p] = 0;
-    }     
+    }
   }
 
   // send LFO output to LED and MIDI
- 
+
   uint8_t switchState = binValOne << 1 | binValTwo;
   switch (switchState) {
     case 0:
-    {
-      lfoPtr = tri;
-      break;
-    }
+      {
+        lfoPtr = tri;
+        break;
+      }
     case 1:
-    {
-      lfoPtr = squ50;
-      break;
-    }
+      {
+        lfoPtr = squ50;
+        break;
+      }
     case 2:
-    {
-      lfoPtr = noise;
-      break;
-    }
+      {
+        lfoPtr = noise;
+        break;
+      }
     case 3:
-    {
-      lfoPtr = sawDown;
-      break;
-    }
+      {
+        lfoPtr = sawDown;
+        break;
+      }
   }
-  
-  volatile uint8_t lfoValue = uint8_t(lfoPtr[lfoPhase] * lfoDepth/1.25); 
+
+  volatile uint8_t lfoValue = uint8_t(lfoPtr[lfoPhase] * lfoDepth / 1.25);
   uint16_t midiLFOValue = uint16_t(lfoValue * 1.25 / 2.0);
   if (midiLFOValue < 0) {
     midiLFOValue = 0;
@@ -287,7 +304,7 @@ void loop()
     midiLFOValue = 127;
   }
   analogWrite(LFO_OUT, lfoValue);
-  msg.data1 = LFO_CC;               
+  msg.data1 = LFO_CC;
   msg.data2 = midiLFOValue;
   Serial.write(msg.commChannel);
   Serial.write(msg.data1);
